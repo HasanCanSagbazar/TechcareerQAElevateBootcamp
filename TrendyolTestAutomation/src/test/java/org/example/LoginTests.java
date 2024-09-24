c5b5afc6c9342b2efb45a98f83b28f3e758b5dee
HasanCanSagbazar <hasancan.sagbazar@outlook.com>
2024-09-24 18:45:03 +0300

    Trendyol Test Automation - TestNG

--- TrendyolTestAutomation/src/test/java/org/example/LoginTests.java
+++ TrendyolTestAutomation/src/test/java/org/example/LoginTests.java
@@ -3,22 +3,50 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class LoginTests {
-    @Test
-    public void LoginSuccesful() throws InterruptedException {
-        WebDriver chromeDriver = new ChromeDriver();
+    WebDriver chromeDriver;
+    Properties prop;
+    @BeforeMethod
+    public void OpenBrowser(){
+        chromeDriver = new ChromeDriver();
         chromeDriver.get("https://www.trendyol.com/giris");
         chromeDriver.manage().window().maximize();
-        chromeDriver.findElement(By.id("login-email")).sendKeys("<email>");
-        chromeDriver.findElement(By.name("login-password")).sendKeys("<password>");
+
+        try {
+            prop = new Properties();
+            InputStream fileInput = getClass().getClassLoader().getResourceAsStream("config.properties");
+            prop.load(fileInput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void LoginSuccesful() throws InterruptedException {
+        String email = prop.getProperty("EMAIL");
+        String password = prop.getProperty("PASSWORD");
+
+        chromeDriver.findElement(By.id("login-email")).sendKeys(email);
+        chromeDriver.findElement(By.name("login-password")).sendKeys(password);
         chromeDriver.findElement(By.cssSelector("button[type='submit']")).click();
         Thread.sleep(1000);
         String value = chromeDriver.findElement(By.cssSelector("[class='link account-user'] p")).getText();
 
-        System.out.println(value);
+        Assert.assertEquals("Hesabım", value);
+    }
 
+    @AfterMethod
+    public void afterTest() throws InterruptedException {
         Thread.sleep(1000);
         chromeDriver.quit();
     }
