a590dd85303e1fa43d925c4e807ac36f4dc0707f
HasanCanSagbazar <hasancan.sagbazar@outlook.com>
2024-09-24 17:25:33 +0300

    Login Website

--- TrendyolTestAutomation/src/main/java/org/example/App.java
+++ TrendyolTestAutomation/src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.trendyol.com/giris");
+        chromeDriver.findElement(By.id("login-email")).sendKeys("canhasan870@gmail.com");
+        chromeDriver.findElement(By.name("login-password")).sendKeys("Ia9b5C3!D2F");
+        chromeDriver.findElement(By.cssSelector("button[type='submit']")).click();
     }
 }
